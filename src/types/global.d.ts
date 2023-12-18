596df3f4cde55840f1c455df386581b93854331f
IvanOrsh <ksironov@mail.ru>
2023-12-19 02:50:16 +0300

    build: implement supabase

---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_SUPABASE_URL: string;
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    }
+  }
+}
+
+export {};
