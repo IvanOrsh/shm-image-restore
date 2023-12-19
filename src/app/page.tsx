249b94807c6d810f658485bda235dd6e565ea4bf
IvanOrsh <ksironov@mail.ru>
2023-12-19 11:35:03 +0300

    fix: incorrect shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
 export default function Home() {
+  const loggedIn = false;
+
+  try {
+    const supabase = createServerComponentClient({ cookies });
+  } catch (error) {
+    console.log(`[home]: ${error}`);
+  }
+
   return <div className="text-lime-700 font-extrabold">Home</div>;
 }
